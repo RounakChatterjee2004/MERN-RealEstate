681832572452958ee5fc66a188caf4ee139fa555
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-07-26 20:25:35 +0530

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <>
-      <h1 className="text-red-500"> App </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-out" element={<SignUp />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
