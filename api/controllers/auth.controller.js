2b4266276d0582036e133ac3db58637c2d3ef1ff
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-08-03 17:04:14 +0530

    created middleware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
-
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = bcrypt.hashSync(password, 12);
@@ -11,9 +11,12 @@ export const signup = async (req, res) => {
     password: hashedPassword,
   });
 
-  await newUser.save();
-
-  res.status(201).json({
-    message: "User created successfully",
-  });
+  try {
+    await newUser.save();
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -22,3 +22,15 @@ mongoose
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth/signup", authRouter);
+
+// middleware to handle errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
