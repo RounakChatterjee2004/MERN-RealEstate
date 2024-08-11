63b740658fb01641273e26e207136e30f86dc672
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-08-11 13:40:19 +0530

    uploaded profile image functionality using firebase storgae

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,35 +1,103 @@
-import React from "react";
+import {
+  uploadBytesResumable,
+  getDownloadURL,
+  getStorage,
+  ref,
+} from "firebase/storage";
+import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
+import { app } from "../firebase.js";
+
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+
+  const [file, setFile] = useState(undefined);
+  const [filePer, setFilePer] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  const storage = getStorage(app);
+
+  const handleFileUpload = (file) => {
+    const fileName = new Date().getTime() + file.name; //unique file name
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file); // for seeing the percentage of upload
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePer(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7 "> Profile </h1>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
         <img
-          src={currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
-
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2 MB)
+            </span>
+          ) : filePer > 0 && filePer < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePer}%`}</span>
+          ) : filePer === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
           id="username"
-          className="border p-3  rounded-lg"
+          className="border p-3 rounded-lg"
         />
         <input
           type="email"
           placeholder="email"
           id="email"
-          className="border p-3  rounded-lg"
+          className="border p-3 rounded-lg"
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           id="password"
-          className="border p-3  rounded-lg"
+          className="border p-3 rounded-lg"
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           Update
@@ -37,8 +105,8 @@ export default function Profile() {
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer"> Delete Account </span>
-        <span className="text-red-700 cursor-pointer"> SignOut </span>
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">SignOut</span>
       </div>
     </div>
   );
