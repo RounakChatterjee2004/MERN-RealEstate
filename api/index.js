8ebc391500416272b45d90741ef3c9e42d9ad3c7
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-08-16 18:35:12 +0530

    create listing api and listing model

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cors from "cors";
+
 import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
@@ -30,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
+app.use("/api/listing", listingRouter);
 // middleware to handle errors
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
---
+++ api/models/listing.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      // rent or sale
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      // which user created the lsiting
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
+// exported from backend to frontend
