f2a614e79fc3b5ec2d99c9886ba793c7d0b39ca4
Rounak Chatterjee <123956143+RounakChatterjee2004@users.noreply.github.com>
2024-08-30 13:32:04 +0530

    rollback

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -11,8 +11,8 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
-router.post("/update/:id", updateUser);
-router.delete("/delete/:id", deleteUser);
-router.get("/listings/:id", getUserListing);
-router.get("/:id", getUser);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
+router.get("/:id", verifyToken, getUser);
 export default router;
